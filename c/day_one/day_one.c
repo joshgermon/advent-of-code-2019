5487a08b2b850e72d9308c0ae13ca019480b91cc
Joshua <joshgermon@gmail.com>
2023-09-08 23:26:00 +1000

    Day 1 Part 2 - C

--- c/day_one/day_one.c
+++ c/day_one/day_one.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]) {
+#define INPUT_FILENAME "input.txt"
 
-    printf("****ADVENT OF CODE 2019 - DAY ONE ****\n");
-    printf("Calulating fuel requirements...\n");
-
+int calculate_fuel_from_mass() {
     FILE* fptr;
 
-    fptr = fopen("input.txt", "r");
+    fptr = fopen(INPUT_FILENAME, "r");
     char line[256];
 
     if (fptr == NULL) {
@@ -19,11 +17,33 @@ int main(int argc, char* argv[]) {
     int sum_of_fuel_needed = 0;
 
     while(fgets(line, sizeof(line), fptr)) {
-        int calculated_fuel = (atoi(line) / 3) - 2;
-        sum_of_fuel_needed += calculated_fuel;
+        // Calculate fuel needed from module mass
+        int calculated_module_fuel = (atoi(line) / 3) - 2;
+        sum_of_fuel_needed += calculated_module_fuel;
+
+        // Calculate fuel needed for fuel required by module mass
+        int calculated_fuel_fuel = calculated_module_fuel / 3 - 2;
+
+        while(calculated_fuel_fuel > 0) {
+            sum_of_fuel_needed += calculated_fuel_fuel;
+            calculated_fuel_fuel = calculated_fuel_fuel / 3 - 2;
+        }
     }
 
     fclose(fptr);
-
-    printf("Calculations completed.\nFuel required: %i\n", sum_of_fuel_needed);
+    return sum_of_fuel_needed;
 }
+
+int main(int argc, char* argv[]) {
+
+    printf("****ADVENT OF CODE 2019 - DAY ONE ****\n");
+    printf("Calulating fuel requirements...\n");
+
+    // Part one calculation method
+    int fuel_needed = calculate_fuel_from_mass();
+
+    printf("Calculations completed.\nFuel required: %i\n", fuel_needed);
+
+    return 0;
+}
+
