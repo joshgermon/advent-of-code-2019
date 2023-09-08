857404e24ae4db966bce7864e4002169d546b5e2
Joshua <joshgermon@gmail.com>
2023-09-08 23:35:00 +1000

    Simplify part 2 calculation logic and tidy up

--- c/day_one/day_one.c
+++ c/day_one/day_one.c
@@ -14,35 +14,38 @@ int calculate_fuel_from_mass() {
         return 1;
     }
 
+    printf("Calulating fuel requirements\n");
+
     int sum_of_fuel_needed = 0;
 
     while(fgets(line, sizeof(line), fptr)) {
-        // Calculate fuel needed from module mass
-        int calculated_module_fuel = (atoi(line) / 3) - 2;
-        sum_of_fuel_needed += calculated_module_fuel;
+        // Read in line and convert to an integer
+        int module_mass = atoi(line);
 
-        // Calculate fuel needed for fuel required by module mass
-        int calculated_fuel_fuel = calculated_module_fuel / 3 - 2;
+        // Calculate intial fuel needed for module mass
+        int calculated_fuel = module_mass / 3 - 2;
 
-        while(calculated_fuel_fuel > 0) {
-            sum_of_fuel_needed += calculated_fuel_fuel;
-            calculated_fuel_fuel = calculated_fuel_fuel / 3 - 2;
+        // Calculate the additional fuel needed for the module required fuel
+        while(calculated_fuel > 0) {
+            sum_of_fuel_needed += calculated_fuel;
+            calculated_fuel = calculated_fuel / 3 - 2;
         }
+        // Progress bar
+        printf(".");
     }
 
     fclose(fptr);
+
+    printf("\nCalculations completed. ✔\n");
     return sum_of_fuel_needed;
 }
 
 int main(int argc, char* argv[]) {
-
     printf("****ADVENT OF CODE 2019 - DAY ONE ****\n");
-    printf("Calulating fuel requirements...\n");
 
-    // Part one calculation method
     int fuel_needed = calculate_fuel_from_mass();
 
-    printf("Calculations completed.\nFuel required: %i\n", fuel_needed);
+    printf("Fuel required: %i\n", fuel_needed);
 
     return 0;
 }
